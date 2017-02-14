b90234f156c8b31ee38a0556886aaa07ec99cd3c
ivann3 <ivann3@RECEPCION>
2017-02-14 12:17:29 +0100

    palntilla Jquery DatePicker

---
+++ EF10/Controllers/CabeceraFrasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EF10.Controllers
+{
+    public class CabeceraFrasController : Controller
+    {
+        // GET: CabeceraFras
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
